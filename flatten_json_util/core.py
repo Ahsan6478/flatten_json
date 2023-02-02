a5e4ddd7183a74bf261661280ec8df762e7e37f0
Ahsan <ahsantahir0@gmail.com>
2023-02-02 14:18:00 +0500

    core: add _flatten_list recursive helper

--- flatten_json_util/core.py
+++ flatten_json_util/core.py
@@ -26,3 +26,29 @@ def _flatten_dict(dict_key, data, c_str=None):
             result[f"{prefix}_{i_key}"] = value
 
     return result
+
+
+def _flatten_list(data, dict_key=None, c_str=None):
+    """Flatten a nested list within a parent container."""
+    result = {}
+    if dict_key is None:
+        dict_key = ""
+        items = data
+    else:
+        items = data[dict_key]
+
+    for num in range(len(items)):
+        value = items[num]
+        if c_str:
+            prefix = f"{c_str}_{dict_key}"
+        else:
+            prefix = str(dict_key)
+
+        if isinstance(value, dict):
+            result.update(_flatten_dict(num, items, c_str=prefix))
+        elif isinstance(value, list):
+            result.update(_flatten_list(items, num, c_str=prefix))
+        else:
+            result[f"{prefix}_{num}"] = value
+
+    return result
