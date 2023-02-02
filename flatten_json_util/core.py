ff32384ee37b8439e233e40185bdf4f4b736d650
Ahsan <ahsantahir0@gmail.com>
2023-02-02 11:30:00 +0500

    core: add _flatten_dict recursive helper

--- flatten_json_util/core.py
+++ flatten_json_util/core.py
@@ -4,3 +4,25 @@
 def _is_scalar(value):
     """Check if a value is a scalar (not a dict or list)."""
     return not isinstance(value, (dict, list))
+
+
+def _flatten_dict(dict_key, data, c_str=None):
+    """Flatten a nested dictionary within a parent container."""
+    result = {}
+    if dict_key is None:
+        dict_key = ""
+    for i_key in data[dict_key].keys():
+        value = data[dict_key][i_key]
+        if c_str:
+            prefix = f"{c_str}_{dict_key}"
+        else:
+            prefix = str(dict_key)
+
+        if isinstance(value, dict):
+            result.update(_flatten_dict(i_key, data[dict_key], c_str=prefix))
+        elif isinstance(value, list):
+            result.update(_flatten_list(data[dict_key], i_key, c_str=prefix))
+        else:
+            result[f"{prefix}_{i_key}"] = value
+
+    return result
