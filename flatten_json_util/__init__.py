24a63930a6109809b9ec43ee9efb3dd6655a8769
Ahsan <ahsantahir0@gmail.com>
2023-02-08 13:15:00 +0500

    init: add lazy imports for export functions

--- flatten_json_util/__init__.py
+++ flatten_json_util/__init__.py
@@ -5,4 +5,25 @@ __all__ = [
     "flatten",
     "flatten_json",
     "flatten_single",
+    "to_csv",
+    "to_excel",
+    "to_dataframe",
 ]
+
+
+def to_csv(data, file_name):
+    """Flatten JSON data and export to CSV. Requires pandas."""
+    from .export import to_csv as _to_csv
+    return _to_csv(data, file_name)
+
+
+def to_excel(data, file_name):
+    """Flatten JSON data and export to Excel. Requires pandas and xlsxwriter."""
+    from .export import to_excel as _to_excel
+    return _to_excel(data, file_name)
+
+
+def to_dataframe(data):
+    """Flatten JSON data and return a pandas DataFrame. Requires pandas."""
+    from .export import to_dataframe as _to_dataframe
+    return _to_dataframe(data)
