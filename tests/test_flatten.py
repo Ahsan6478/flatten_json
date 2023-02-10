ce0f6b6e70a927711eb8b6eb9c12c1c80ebdd96a
Ahsan <ahsantahir0@gmail.com>
2023-02-10 14:45:00 +0500

    tests: add edge case tests for deeply nested and mixed types

--- tests/test_flatten.py
+++ tests/test_flatten.py
@@ -18,3 +18,17 @@ class TestFlattenSingle:
         data = {"scores": [10, 20, 30]}
         result = flatten_single(data)
         assert result == [{"scores_0": 10, "scores_1": 20, "scores_2": 30}]
+
+    def test_deeply_nested(self):
+        data = {"a": {"b": {"c": "deep"}}}
+        result = flatten_single(data)
+        assert result == [{"a_b_c": "deep"}]
+
+    def test_mixed_types(self):
+        data = {"name": "test", "count": 5, "active": True, "value": None}
+        result = flatten_single(data)
+        assert result == [{"name": "test", "count": 5, "active": True, "value": None}]
+
+    def test_empty_dict(self):
+        result = flatten_single({})
+        assert result == [{}]
